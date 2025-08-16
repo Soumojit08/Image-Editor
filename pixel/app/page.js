016a22d863dc98a783dc3502862e23a4c58aa44b
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-08-16 22:56:01 +0530

    check shadcn working

--- pixel/app/page.js
+++ pixel/app/page.js
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       Hello
+      <div>
+        <Button size="sm" disabled>
+          <Loader2Icon className="animate-spin" />
+          Please wait
+        </Button>
+      </div>
     </div>
   );
 }
