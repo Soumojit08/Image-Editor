2f1509b6274b5ba20a0ef0c9c9231835185bc2ba
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-09-18 10:33:56 +0530

    added background

--- RecipeHub/frontend/src/App.jsx
+++ RecipeHub/frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import LightRays from "./components/LightRays";
+
 const App = () => {
   return (
-    <div className="text-4xl font-sans dark:bg-background text-foreground">
-      App
+    <div className="relative h-full w-full font-sans dark:bg-background text-foreground ">
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#00ffff"
+        raysSpeed={1.5}
+        lightSpread={0.8}
+        rayLength={1.2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0.1}
+        distortion={0.05}
+        className="custom-rays"
+      />
     </div>
   );
 };
